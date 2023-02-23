866f04ad8ea12edd05c2fba0db3ba7450c7abc83
guojiankun <guojiankun@egridcloud.com>
2023-02-23 23:39:37 +0800

    debug docs

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,17 +5,17 @@ module.exports = {
   dest: path.resolve(__dirname, "../../dist"),
   title: "GzCom",
   description: "封装平时搭建的通用组件",
-  plugins: ["demo-container"],
+  // plugins: ["demo-container"],
   themeConfig: {
     logo: "/logo.gif",
     sidebarDepth: 0,
     sidebar: [
       ["/", "快速上手"],
-      ["/AsyncMessageBox/", "AsyncMessageBox"],
-      ["/Dialog/", "Dialog"],
+      // ["/AsyncMessageBox/", "AsyncMessageBox"],
+      // ["/Dialog/", "Dialog"],
       ["/Group/", "Group"],
-      ["/DatePickerRange/", "DatePickerRange"],
-      ["/Table/", "Table"],
+      // ["/DatePickerRange/", "DatePickerRange"],
+      // ["/Table/", "Table"],
     ],
     nav: [
       { text: 'Github', link: 'https://github.com/Guosugaz/kun-el-com', target:'_blank' }
